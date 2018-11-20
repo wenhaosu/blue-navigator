a6571851726009a86fe29dab0ec11e7da4636ac9
OhioSucks <zhhan@umich.edu>
2018-11-20 17:48:48 -0500

    Finish db

--- src/components/searchbox.js
+++ src/components/searchbox.js
@@ -4,6 +4,7 @@ import {faArrowCircleRight} from '@fortawesome/free-solid-svg-icons';
 import {withRouter} from 'react-router-dom';
 import './searchbox.css';
 import Select from "react-styled-select";
+import rooms from "../static/locations";
 
 const buildingOptions = [
     {value: 'bbb', label: 'Bob and Betty Beyster Building'},
@@ -13,25 +14,39 @@ const buildingOptions = [
     {value: 'duder', label: 'Duderstadt Center'}
 ];
 
-const roomOptions = [
-    {value: '1670', label: '1670'},
-    {value: '1690', label: '1690'},
-    {value: '2870', label: '2870'},
-    {value: '3777', label: '3777'},
-    {value: '4773', label: '4773'}
-];
+// const roomOptions = [
+//     {value: '1670', label: '1670'},
+//     {value: '1690', label: '1690'},
+//     {value: '2870', label: '2870'},
+//     {value: '3777', label: '3777'},
+//     {value: '4773', label: '4773'}
+// ];
 
 class Searchbox extends Component {
     constructor(props) {
         super(props);
-        this.state = {buildingValue: '', roomValue: ''};
+        this.state = {buildingValue: '', roomValue: '', roomOptions:[]};
 
         this.handleBuildingChange = this.handleBuildingChange.bind(this);
         this.handleRoomChange = this.handleRoomChange.bind(this);
+
     }
 
     handleBuildingChange(value) {
         this.setState({buildingValue: value});
+        if (value == 'bbb') {
+            let options = [];
+            for (var key in rooms["rooms"]) {
+                let temp = {};
+                temp["value"] = rooms["rooms"][key]["lname"];
+                temp["label"] = rooms["rooms"][key]["lname"];
+                options.push(temp);
+            }
+            this.setState({roomOptions: options})
+        }
+        else {
+            this.setState({roomOptions: []})
+        }
     }
 
     handleRoomChange(value) {
@@ -46,7 +61,7 @@ class Searchbox extends Component {
                 <Select className="dark-theme search-building" options={buildingOptions}
                         placeholder="Select Your Building" value={this.state.buildingValue}
                         onChange={this.handleBuildingChange}/>
-                <Select className="dark-theme search-room" options={roomOptions}
+                <Select className="dark-theme search-room" options={this.state.roomOptions}
                         placeholder="Select Your Room"
                         value={this.state.roomValue} onChange={this.handleRoomChange}/>
                 <FontAwesomeIcon
