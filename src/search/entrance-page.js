38b0a4e2eb98f7f7165d13c24b3498acb3051e44
OhioSucks <zhhan@umich.edu>
2018-11-20 21:19:22 -0500

    entrance page changed

--- src/search/entrance-page.js
+++ src/search/entrance-page.js
@@ -8,19 +8,29 @@ import Select from "react-styled-select";
 import {Scrollbars} from 'react-custom-scrollbars';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faAngleDoubleDown} from '@fortawesome/free-solid-svg-icons';
+import rooms from "../static/locations";
 
-const roomOptions = [
-    {value: '1670', label: '1670'},
-    {value: '1690', label: '1690'},
-    {value: '2870', label: '2870'},
-    {value: '3777', label: '3777'},
-    {value: '4773', label: '4773'}
-];
+// const roomOptions = [
+//     {value: '1670', label: '1670'},
+//     {value: '1690', label: '1690'},
+//     {value: '2870', label: '2870'},
+//     {value: '3777', label: '3777'},
+//     {value: '4773', label: '4773'}
+// ];
 
 class EntrancePage extends Component {
     constructor(props) {
         super(props);
-        this.state = {entrances: undefined, roomValue: '', colorMat: [0, 0, 0, 0]};
+
+        let options = [];
+        for (let key in rooms) {
+            let temp = {};
+            temp["value"] = key;
+            temp["label"] = key;
+            options.push(temp);
+        }
+
+        this.state = {entrances: undefined, roomValue: '', colorMat: [0, 0, 0, 0], roomOptions: options};
         this.handleRoomChange = this.handleRoomChange.bind(this);
         this.setActiveColor = this.setActiveColor.bind(this);
     }
@@ -28,6 +38,8 @@ class EntrancePage extends Component {
     handleRoomChange(value) {
         this.setState({roomValue: value, colorMat: [0, 0, 0, 1]});
         this.props.setFunc(value);
+
+
     }
 
     componentDidMount() {
@@ -56,6 +68,19 @@ class EntrancePage extends Component {
                     <div className="page-title">
                         Select your Entrance
                     </div>
+
+                    <div className="in-building">
+                        <div className="in-building-box"
+                            style={{boxShadow: `0 0 10px 5px ${this.state.colorMat[3] === 1 ? "#6293cd" : "#cdcdcd"}`}}>
+                            <div className="in-building-box-title">
+                                Already in this building?
+                            </div>
+                            <Select className="in-building-box-search" options={this.state.roomOptions}
+                                    placeholder="Select Your Current Nearest Room" value={this.state.roomValue}
+                                    onChange={this.handleRoomChange} />
+                        </div>
+                    </div>
+
                     <div style={{marginTop: '25vh', width: '100%', padding: '0 2%', boxSizing: 'border-box'}}>
                         <Scrollbars style={{height: '50vh', width: '100%', whiteSpace: 'nowrap'}}>
                             <div className="entrance-box-container">
@@ -82,17 +107,7 @@ class EntrancePage extends Component {
                         </Scrollbars>
                     </div>
                 </div>
-                <div className="in-building">
-                    <div className="in-building-box"
-                         style={{boxShadow: `0 0 10px 5px ${this.state.colorMat[3] === 1 ? "#6293cd" : "#cdcdcd"}`}}>
-                        <div className="in-building-box-title">
-                            Already in this building?
-                        </div>
-                        <Select className="in-building-box-search" options={roomOptions}
-                                placeholder="Select Your Current Nearest Room" value={this.state.roomValue}
-                                onChange={this.handleRoomChange} />
-                    </div>
-                </div>
+            
                 <FontAwesomeIcon
                     className="next-icon" icon={faAngleDoubleDown} size="5x"
                         // onMouseOver={
